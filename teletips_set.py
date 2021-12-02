bc92900b7c866d6c7d2be8b0ae3d23b99d30cb3e
vullkan <vullkan@gmail.com>
2021-12-02 20:11:06 +0100

    Update teletips_set.py

--- teletips_set.py
+++ teletips_set.py
@@ -1,210 +1,54 @@
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
-#You are free to use this code in any of your project, but you MUST include the following in your README.md (Copy & paste)
-# ##Credits - [Countdown Timer Telegram bot by TeLe TiPs] (https://github.com/teletips/CountdownTimer-TeLeTiPs)
-
-# Changing the code is not allowed! Read GNU AFFERO GENERAL PUBLIC LICENSE: https://github.com/teletips/CountdownTimer-TeLeTiPs/blob/main/LICENSE
- 
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-import asyncio
-from plugins.teletips_t import *
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.raw.functions.messages import UpdatePinnedMessage
+PORT = int(os.environ.get('PORT', 5000))
 
-bot=Client(
-    "Countdown-TeLeTiPs",
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-    bot_token = os.environ["BOT_TOKEN"]
-)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-stoptimer = False
+logger = logging.getLogger(__name__)
+TOKEN = '2146791213:AAHpXJTW8zCp1yvtFcAfl_x5wH9xPragP5o'
 
-TELETIPS_MAIN_MENU_BUTTONS = [
-            [
-                InlineKeyboardButton('❓ HELP', callback_data="HELP_CALLBACK")
-            ],
-            [
-                InlineKeyboardButton('👥 GROUP', callback_data="GROUP_CALLBACK"),
-                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/teletipsofficialchannel'),
-                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/thakshakar')
-            ],
-            [
-                InlineKeyboardButton('➕ CREATE YOUR BOT ➕', callback_data="TUTORIAL_CALLBACK")
-            ]
-        ]
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /website is issued."""
+    update.message.reply_text('noshitcoin.io')
 
-@bot.on_message(filters.command(['start','help']) & filters.private)
-async def start(client, message):
-    text = START_TEXT
-    reply_markup = InlineKeyboardMarkup(TELETIPS_MAIN_MENU_BUTTONS)
-    await message.reply(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True
-    )
+def help(update, context):
+    """Send a message when the command /contract is issued."""
+    update.message.reply_text('0x53F042f3e809d2DcC9492dE2DbF05d1DA0EF5fbb')
+    
+    def help(update, context):
+    """Send a message when the command /audit is issued."""
+    update.message.reply_text('https://github.com/TechRate/Smart-Contract-Audits/blob/main/November/NOSHIT.pdf')
 
-@bot.on_callback_query()
-async def callback_query(client: Client, query: CallbackQuery):
-    if query.data=="HELP_CALLBACK":
-        TELETIPS_HELP_BUTTONS = [
-            [
-                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK")
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(TELETIPS_HELP_BUTTONS)
-        try:
-            await query.edit_message_text(
-                HELP_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass
 
-    elif query.data=="GROUP_CALLBACK":
-        TELETIPS_GROUP_BUTTONS = [
-            [
-                InlineKeyboardButton("🇱🇰 Anytime Any Qs", url="https://t.me/AnytimeAnyQs")
-            ],
-            [
-                InlineKeyboardButton("🌎 TeLe TiPs", url="https://t.me/teletipsofficialontopicchat")
-            ],
-            [
-                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(TELETIPS_GROUP_BUTTONS)
-        try:
-            await query.edit_message_text(
-                GROUP_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass    
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-    elif query.data=="TUTORIAL_CALLBACK":
-        TELETIPS_TUTORIAL_BUTTONS = [
-            [
-                InlineKeyboardButton("🎥 Video", url="https://t.me/TeLeTiPsOfficialChannel/462")
-            ],
-            [
-                InlineKeyboardButton("⬅️ BACK", callback_data="START_CALLBACK"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(TELETIPS_TUTORIAL_BUTTONS)
-        try:
-            await query.edit_message_text(
-                TUTORIAL_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass      
-          
-    elif query.data=="START_CALLBACK":
-        TELETIPS_START_BUTTONS = [
-            [
-                InlineKeyboardButton('❓ HELP', callback_data="HELP_CALLBACK")
-            ],
-            [
-                InlineKeyboardButton('👥 GROUP', callback_data="GROUP_CALLBACK"),
-                InlineKeyboardButton('📣 CHANNEL', url='https://t.me/teletipsofficialchannel'),
-                InlineKeyboardButton('👨‍💻 CREATOR', url='https://t.me/thakshakar')
-            ],
-            [
-                InlineKeyboardButton('➕ CREATE YOUR BOT ➕', callback_data="TUTORIAL_CALLBACK")
-            ]
-        ]
-        reply_markup = InlineKeyboardMarkup(TELETIPS_START_BUTTONS)
-        try:
-            await query.edit_message_text(
-                START_TEXT,
-                reply_markup=reply_markup
-            )
-        except MessageNotModified:
-            pass    
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-@bot.on_message(filters.command('set'))
-async def set_timer(client, message):
-    global stoptimer
-    try:
-        if message.chat.id>0:
-            return await message.reply('⛔️ Try this command in a **group chat**.')
-        elif not (await client.get_chat_member(message.chat.id,message.from_user.id)).can_manage_chat:
-            return await message.reply('👮🏻‍♂️ Sorry, **only admins** can execute this command.')    
-        elif len(message.command)<3:
-            return await message.reply('❌ **Incorrect format.**\n\n✅ Format should be like,\n<code> /set seconds "event"</code>\n\n**Example**:\n <code>/set 86400 "TIME LEFT UNTIL NEW YEAR"</code>')    
-        else:
-            user_input_time = int(message.command[1])
-            user_input_event = str(message.command[2])
-            get_user_input_time = await bot.send_message(message.chat.id, user_input_time)
-            await get_user_input_time.pin()
-            if stoptimer: stoptimer = False
-            if 0<user_input_time<=10:
-                while user_input_time and not stoptimer:
-                    s=user_input_time%60
-                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, s)
-                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
-                    await asyncio.sleep(1)
-                    user_input_time -=1
-                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
-            elif 10<user_input_time<60:
-                while user_input_time>0 and not stoptimer:
-                    s=user_input_time%60
-                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, s)
-                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
-                    await asyncio.sleep(3)
-                    user_input_time -=3
-                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
-            elif 60<=user_input_time<3600:
-                while user_input_time>0 and not stoptimer:
-                    m=user_input_time%3600//60
-                    s=user_input_time%60
-                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, m, s)
-                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
-                    await asyncio.sleep(3)
-                    user_input_time -=3
-                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
-            elif 3600<=user_input_time<86400:
-                while user_input_time>0 and not stoptimer:
-                    h=user_input_time%(3600*24)//3600
-                    m=user_input_time%3600//60
-                    s=user_input_time%60
-                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**h** : {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, h, m, s)
-                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
-                    await asyncio.sleep(7)
-                    user_input_time -=7
-                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
-            elif user_input_time>=86400:
-                while user_input_time>0 and not stoptimer:
-                    d=user_input_time//(3600*24)
-                    h=user_input_time%(3600*24)//3600
-                    m=user_input_time%3600//60
-                    s=user_input_time%60
-                    Countdown_TeLe_TiPs='{}\n\n⏳ {:02d}**d** : {:02d}**h** : {:02d}**m** : {:02d}**s**\n\n<i>"Your **Time** Is Limited, So Don\'t Waste It Living Someone Else\'s Life"</i>\n      - Steve Jobs'.format(user_input_event, d, h, m, s)
-                    finish_countdown = await get_user_input_time.edit(Countdown_TeLe_TiPs)
-                    await asyncio.sleep(9)
-                    user_input_time -=9
-                await finish_countdown.edit("🚨 Beep! Beep!! **TIME'S UP!!!**")
-            else:
-                await get_user_input_time.edit(f"🤷🏻‍♂️ I can't countdown from {user_input_time}")
-                await get_user_input_time.unpin()
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
 
-@bot.on_message(filters.command('stopc'))
-async def stop_timer(Client, message):
-    global stoptimer
-    try:
-        if (await bot.get_chat_member(message.chat.id,message.from_user.id)).can_manage_chat:
-            stoptimer = True
-            await message.reply('🛑 Countdown stopped.')
-        else:
-            await message.reply('👮🏻‍♂️ Sorry, **only admins** can execute this command.')
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-print("Countdown Timer is alive!")
-bot.run()
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
-#Copyright ©️ 2021 TeLe TiPs. All Rights Reserved
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
